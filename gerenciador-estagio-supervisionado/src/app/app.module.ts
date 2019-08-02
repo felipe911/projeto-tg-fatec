4e2a1e763a3b1aac69115a3830d194eb691eda82
felipe911 <felipe@20911@gmail.com>
2019-08-02 20:58:59 -0300

    Adicionando Módulo Inicial de Cadastro

--- gerenciador-estagio-supervisionado/src/app/app.module.ts
+++ gerenciador-estagio-supervisionado/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlunosComponent } from './cadastros/alunos/alunos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunosComponent
   ],
   imports: [
     BrowserModule
