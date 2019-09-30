98ca38936bf1e320f7e1fc0540dceebded25c189
felipe911 <felipe@20911@gmail.com>
2019-09-29 22:37:45 -0300

    Tela Relatório Parcial

---
+++ gerenciador-estagio-supervisionado/src/app/estagio/relatorio-parcial/relatorio-parcial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-relatorio-parcial',
+  templateUrl: './relatorio-parcial.component.html',
+  styleUrls: ['./relatorio-parcial.component.css']
+})
+export class RelatorioParcialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
