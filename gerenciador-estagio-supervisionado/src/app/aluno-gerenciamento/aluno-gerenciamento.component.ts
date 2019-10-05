f16f963ba10e550cba0bcb23e1d5610159b32525
felipe911 <felipe@20911@gmail.com>
2019-10-05 16:08:11 -0300

    Adicionando rota do botão Tipo Atividade

--- gerenciador-estagio-supervisionado/src/app/aluno-gerenciamento/aluno-gerenciamento.component.ts
+++ gerenciador-estagio-supervisionado/src/app/aluno-gerenciamento/aluno-gerenciamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-aluno-gerenciamento',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlunoGerenciamentoComponent implements OnInit {
 
-  constructor() { }
+  
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  getTipoAtividade(){
+    this.router.navigate(['estagio/tipo-atividade']);
+  }
 }
